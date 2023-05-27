2d4fb18e50645a631576ba3152fa05739763ac89
Anmol Kaloni <98876016+e19cse213@users.noreply.github.com>
2023-05-27 17:48:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000")
+})
